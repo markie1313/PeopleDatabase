873306bde9a21a1c21a5e7082e4c6a5bade71b91
markie1313 <markie13@outlook.com>
2016-03-29 12:29:32 -0500

    Initial Commit

---
+++ PeopleDatabase/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  PeopleDatabase
+//
+//  Created by Mark Lindberg on 3/29/16.
+//  Copyright © 2016 Phredster. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
